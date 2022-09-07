a3f4fe23a35920c5162009bda37a98257374b901
gonzaga82 <gonzalo.moreno@odigeo.com>
2022-09-07 19:25:32 +0200

    first commit

---
+++ components/TheHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <ul>
+        <li>Home</li>
+        <li>Buscador de vídeos</li>
+        <li>Contacto</li>
+    </ul>
+</template>
+<style scoped>
+ul {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    list-style: none;
+    gap: 1%;
+
+}
+
+li {
+    flex-basis: 98%;
+    text-align: center;
+}
+</style>
